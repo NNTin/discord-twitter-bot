7ccd65f3263be7c77d8bb33745060c3e9c58fb55
NNTin <nguyen.ngoctindaniel@gmail.com>
2017-03-24 17:11:02 +0100

    reorganized
    using default on_data() now. Discord message sending happens in
    on_status().

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Post tweets to Discord Webhook. It can be filtered by tracking keywords or by fo
 - [ ] If retweeted: identify it as a retweet
 - [ ] If picture(s) provided: send pictures in into text channel
 ### Core
-- [ ] use [default on_data()](https://github.com/tweepy/tweepy/blob/master/tweepy/streaming.py#L45)
-- [ ] handle tweets in on_status()
+- [x] use [default on_data()](https://github.com/tweepy/tweepy/blob/master/tweepy/streaming.py#L45)
+- [x] handle tweets in on_status()
 - [ ] utilize on_connect(), on_event(), on_direct_message(), etc.
 - [ ] twitter handle to twitter id converter
 - [ ] extract twitter IDs from [rokxx's dota 2 twitter list](https://twitter.com/rokxx/lists/dota-2/members)
--- main.py
+++ main.py
@@ -1,41 +1,90 @@
 #Import the necessary methods from tweepy library
 from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
+from tweepy import OAuthHandler, Stream
 from discordWebhooks import Webhook, Attachment, Field
-import calendar
-import time
-import json
-import secret
+import calendar, time, secret
+
+followedTwitterIDs = ['3065618342']
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
-    def on_data(self, data):
+    def on_status(self, status):
+        """Called when a new status arrives"""
         try:
-            data = json.loads(data)
-            print(data['created_at'])
+            data = status._json
+            print(data)
+            if data['user']['id_str'] in followedTwitterIDs or data['user']['id_str'] not in followedTwitterIDs: #filter out random people replying to Dota 2 personalities
 
-            wh = Webhook(url=secret.WEBHOOK_URL, username="Chirp",
-                         icon_url="http://cdn.dota2.com/apps/dota2/images/heroes/rattletrap_lg.png")
-            at = Attachment(author_name=data['user']['screen_name'],
-                            author_icon=data['user']['profile_image_url'].replace('\\', ''),
-                            color="#ffffff", pretext=data['text'].replace('\\', ''),
-                            title_link="https://twitter.com/" + data['user']['screen_name'] + "/status/" + str(data['id_str']),
-                            footer="Tweet created at",
-                            footer_icon="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697029-twitter-512.png",
-                            ts=calendar.timegm(time.strptime(data['created_at'], '%a %b %d %H:%M:%S +0000 %Y')))
-
-            wh.addAttachment(at)
-            wh.post()
+                wh = Webhook(url=secret.WEBHOOK_URL, username="Chirp",
+                             icon_url="http://cdn.dota2.com/apps/dota2/images/heroes/rattletrap_lg.png")
+                at = Attachment(author_name=data['user']['screen_name'],
+                                author_icon=data['user']['profile_image_url'].replace('\\', ''),
+                                color="#ffffff", pretext=data['text'].replace('\\', ''),
+                                title_link="https://twitter.com/" + data['user']['screen_name'] + "/status/" + str(data['id_str']),
+                                footer="Tweet created at",
+                                footer_icon="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697029-twitter-512.png",
+                                ts=calendar.timegm(time.strptime(data['created_at'], '%a %b %d %H:%M:%S +0000 %Y')))
+                wh.addAttachment(at)
+                wh.post()
         except:
             print(data)
             print(type(data))
-            raise Exception("TODO: Analyze error (if it exists).")
-
         return True
 
-    def on_error(self, status):
+
+    def on_delete(self, status_id, user_id):
+        """Called when a delete notice arrives for a status"""
+        print('on_delete')
+        print(status_id)
+        print(user_id)
+        return
+
+    def on_event(self, status):
+        """Called when a new event arrives"""
+        print('on_event')
         print(status)
+        return
+
+    def on_direct_message(self, status):
+        """Called when a new direct message arrives"""
+        print('on_direct_message')
+        print(status)
+        return
+
+    def on_friends(self, friends):
+        """Called when a friends list arrives.
+        friends is a list that contains user_id
+        """
+        print('on_friends')
+        print(friends)
+        return
+
+    def on_limit(self, track):
+        """Called when a limitation notice arrives"""
+        print('on_limit')
+        print(track)
+        return
+
+    def on_error(self, status_code):
+        """Called when a non-200 status code is returned"""
+        print('on_error')
+        print(status_code)
+        return False
+
+    def on_disconnect(self, notice):
+        """Called when twitter sends a disconnect notice
+        Disconnect codes are listed here:
+        https://dev.twitter.com/docs/streaming-apis/messages#Disconnect_messages_disconnect
+        """
+        print('on_disconnect')
+        print(notice)
+        return
+
+    def on_warning(self, notice):
+        """Called when a disconnection warning message arrives"""
+        print('on_warning')
+        print(notice)
+        return
 
 
 if __name__ == '__main__':
@@ -46,5 +95,11 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['soccer', 'basketball'])
-    #stream.filter(follow=['<twitter id>', '<twitter id>'])
+    #stream.filter(track=['soccer', 'basketball'])
+    while True:
+        try:
+            #stream.filter(follow=followedTwitterIDs)
+            stream.filter(track=['soccer', 'basketball'])
+        except:
+            time.sleep(5)
+            print('restarting')
