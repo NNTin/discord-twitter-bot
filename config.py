2af85f1f488f980de437af971eaa42a5b95f4587
NNTin <nguyen.ngoctindaniel@gmail.com>
2018-09-14 14:52:25 +0200

    optional custom message and optional keyword filtering (#22)
    * custom message and keyword
    
    * remove junk
    
    * bug fix

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 from dataIO import fileIO
 import os
 
-# todo: add mongoDB support
-
-false_strings = ["false", "False", "f", "F", "0", ""]
+false_strings = ["false", "False", "f", "F", "0", "", "n", "N", "no", "No", "NO", "FALSE"]
 
 if fileIO("data.json", "check"):
     data_json = fileIO("data.json", "load")
@@ -20,9 +18,14 @@ else:
                 "IncludeReplyToUser": False if os.environ["INCLUDE_REPLY_TO_USER"] in false_strings else True,
                 "IncludeRetweet": False if os.environ["INCLUDE_RETWEET"] in false_strings else True,
                 "IncludeUserReply": False if os.environ["INCLUDE_USER_REPLY"] in false_strings else True,
-                "webhook_urls": os.environ.get("WEBHOOK_URL", []).replace(" ", "").split(","),
-                "twitter_ids": os.environ.get("TWITTER_ID", []).replace(" ", "").split(",")
+                "webhook_urls": os.environ["WEBHOOK_URL"].replace(" ", "").split(","),
+                "twitter_ids": os.environ["TWITTER_ID"].replace(" ", "").split(","),
+                "custom_message": os.environ.get("CUSTOM_MESSAGE", None)
             }
         ],
         "twitter_ids": []
     }
+    keyword_sets = os.environ.get("KEYWORDS", None)
+    if keyword_sets:
+        keyword_sets = [keyword_set.split("+") for keyword_set in keyword_sets.replace(" ", "").split(",")]
+        data_json["Discord"]["keyword_sets"] = keyword_sets
